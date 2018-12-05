4297d644a1e3ca52197d829c520575deb81bbe43
Simon Janezic <simon.janezic@gmail.com>
2018-12-05 14:39:37 +0100

    Add dynamic part of the visualisation. Refactor old API to better fit with dynamic part.

--- lib/text_exploration/doc_explorer.py
+++ lib/text_exploration/doc_explorer.py
@@ -7,7 +7,7 @@ from sklearn.cluster import DBSCAN
 import numpy as np
 from matplotlib import pyplot as plt
 import re
-import matplotlib.cm as cm
+from sklearn.neighbors import KDTree
 
 NON_ALPHABETIC_REGEX = re.compile('[^a-zA-Z]')
 STOP_WORDS = set(stopwords.words('english'))
@@ -20,22 +20,36 @@ def remove_non_alphabetic(text):
 class DocExplorer():
     """ Visualisation tool for static and dynamic exploration of documents. """
 
+    STAT_FONT_SIZE_MAX = 16
+    STAT_FONT_SIZE_MIN = 9
+    DYNM_FONT_SIZE = 12
+
     def __init__(self, method='tfidf', n_keywords_static=3,
-                 n_keywords_dynamic=5):
+                 n_keywords_dynamic=5, fig_size=(12, 10)):
         """
         :param method: Method to use for keyword extraction. Either 'tfidf' or 'g2'
         :param n_keywords_static: Number of keywords to display per cluster
         :param n_keywords_dynamic: Number of keywords to display for lense exploration
+        :param fig_size: Figure size.
         """
         self.method = method
+        self.extract_keywords = self._keywords_tfidf if self.method == 'tfidf' else self._keywords_g2
         self.n_keywords_static = n_keywords_static
         self.n_keywords_dynamic = n_keywords_dynamic
+
+        _, self.ax = plt.subplots(figsize=fig_size)
+        self.scatter_plot = None
 
         self.tf_matrix = None
         self.tf_feature_names = None
 
         self.X_em = None
         self.clusters = None
+        self.kd_tree = None
+
+        self.tf_totals_words = None
+        self.tf_totals_documents = None
+        self.tf_expected = None
 
     def fit(self, docs, X_em=None, clusters=None):
         """
@@ -53,6 +67,12 @@ class DocExplorer():
         self.tf_matrix = tf_vectorizer.fit_transform(docs).toarray()
         self.tf_feature_names = np.array(tf_vectorizer.get_feature_names())
 
+        # G2 method specific caching
+        if self.method == 'g2':
+            self.tf_totals_words = self.tf_matrix.sum(axis=0)
+            self.tf_totals_documents = self.tf_matrix.sum(axis=1)
+            self.tf_expected = self.tf_totals_words / self.tf_totals_words.sum()
+
         if X_em is None:
             self.X_em = TSNE().fit_transform(self.tf_matrix)
         else:
@@ -63,67 +83,64 @@ class DocExplorer():
         else:
             self.clusters = clusters
 
-    def _keywords_tfidf(self, clusters):
+        self.kd_tree = KDTree(self.X_em, leaf_size=20)
+
+    def _keywords_tfidf(self, is_in_cluster, n_keywords):
         """
-        :return: Keywords for each cluster using tf-idf method.
+        Get keywords for a given cluster using tf-idf method.
+        :param is_in_cluster: boolean array of shape (n_samples, ) that indicates cluster membership
+        :param n_keywords: number of keywords to extract.
+        :return: list of keywords
         """
-        keywords = dict()
-        for c in np.unique(clusters):
-            keywords_idx = np.argsort(np.sum(self.tf_matrix[clusters == c, :], axis=0))[
-                           -self.n_keywords_static:]
-            keywords[c] = list(reversed(self.tf_feature_names[keywords_idx]))
-        return keywords
+        keywords_idx = np.argsort(np.sum(self.tf_matrix[is_in_cluster, :], axis=0))[
+                       -n_keywords:]
+        return list(reversed(self.tf_feature_names[keywords_idx]))
 
-    def _keywords_g2(self, clusters):
+    def _keywords_g2(self, is_in_cluster, n_keywords):
         """
-        :return: Keywords for each cluster using G2 method.
+        Get keywords for a given cluster using g2 method.
+        :param is_in_cluster: boolean array of shape (n_samples, ) that indicates cluster membership
+        :param n_keywords: number of keywords to extract.
+        :return: list of keywords
         """
-        tf_totals_words = self.tf_matrix.sum(axis=0)
-        tf_totals_documents = self.tf_matrix.sum(axis=1)
-        tf_expected = tf_totals_words / tf_totals_words.sum()
+        expected_in_cluster = self.tf_totals_documents[is_in_cluster].sum() * self.tf_expected
+        expected_out_cluster = self.tf_totals_documents[~is_in_cluster].sum() * self.tf_expected
+        tf_in_cluster = self.tf_matrix[is_in_cluster, :].sum(axis=0)
+        tf_out_cluster = self.tf_totals_words - tf_in_cluster
 
-        keywords = dict()
-        for c in np.unique(clusters):
-            is_in_cluster = clusters == c
-            expected_in_cluster = tf_totals_documents[is_in_cluster].sum() * tf_expected
-            expected_out_cluster = tf_totals_documents[~is_in_cluster].sum() * tf_expected
-            tf_in_cluster = self.tf_matrix[is_in_cluster, :].sum(axis=0)
-            tf_out_cluster = tf_totals_words - tf_in_cluster
+        g2 = 2 * (tf_in_cluster * np.log((1e-10 + tf_in_cluster) / expected_in_cluster) +
+                  (tf_out_cluster * np.log((1e-10 + tf_out_cluster) / expected_out_cluster)))
 
-            g2 = 2 * (tf_in_cluster * np.log((1e-10 + tf_in_cluster) / expected_in_cluster) +
-                      (tf_out_cluster * np.log((1e-10 + tf_out_cluster) / expected_out_cluster)))
+        keywords_idx = np.argsort(g2)[-n_keywords:]
+        return list(reversed(self.tf_feature_names[keywords_idx]))
 
-            keywords_idx = np.argsort(g2)[-self.n_keywords_static:]
-            keywords[c] = list(reversed(self.tf_feature_names[keywords_idx]))
-        return keywords
-
-    def plot_static(self, fig_size=(12, 10)):
+    def plot_static(self):
         """
         Plots static labels for clusters of the embedding.
-        :param size: figure size
         """
-        extract_keywords = self._keywords_tfidf if self.method == 'tfidf' else self._keywords_g2
-        keywords = extract_keywords(self.clusters)
-
-        f, ax = plt.subplots(figsize=fig_size)
-        ax.scatter(self.X_em[:, 0], self.X_em[:, 1], c='gray', edgecolors='black')
+        self.scatter_plot = self.ax.scatter(self.X_em[:, 0], self.X_em[:, 1], facecolor=(0.5, 0.5, 0.5, 1),
+                                            edgecolor=(0, 0, 0, 1))
+        self.ax.axis('equal')
 
         for c in np.unique(self.clusters):
             if c < 0:
                 continue
 
-            x_avg, y_avg = np.mean(self.X_em[self.clusters == c, :], axis=0)
-            font_sizes = np.linspace(16, 9, num=self.n_keywords_static)
+            is_in_cluster = self.clusters == c
+            keywords = self.extract_keywords(is_in_cluster, self.n_keywords_static)
+
+            x_avg, y_avg = np.mean(self.X_em[is_in_cluster, :], axis=0)
+            font_sizes = np.linspace(self.STAT_FONT_SIZE_MAX, self.STAT_FONT_SIZE_MIN, num=self.n_keywords_static)
             dy = [0, 0]
-            for i, keyword in enumerate(keywords[c]):
+            for i, keyword in enumerate(keywords):
                 annotation_side = (i % 2 * 2 - 1)
                 dy[i % 2] += annotation_side * font_sizes[i] * 0.6
-                ax.annotate(keyword, (x_avg, y_avg), (0, dy[i % 2]), textcoords='offset points', ha='center',
-                            va='center', fontsize=font_sizes[i],
-                            bbox=dict(boxstyle='square,pad=0.1', facecolor='red', alpha=0.5, linewidth=0),
-                            color='white', fontweight='bold')
+                self.ax.annotate(keyword, (x_avg, y_avg), (0, dy[i % 2]), textcoords='offset points', ha='center',
+                                 va='center', fontsize=font_sizes[i],
+                                 bbox=dict(boxstyle='square,pad=0.1', facecolor='red', alpha=0.5, linewidth=0),
+                                 color='white', fontweight='bold')
                 dy[i % 2] += annotation_side * font_sizes[i] * 0.6
-        return ax
+        return self.ax
 
     def plot_dynamic(self, x, y, r):
         """
@@ -132,7 +149,28 @@ class DocExplorer():
         :param y: y coordinate of the lens.
         :param r: radius of the lens.
         """
-        pass
+        is_selected = np.zeros(self.tf_matrix.shape[0], bool)
+        selected_idx = self.kd_tree.query_radius([[x, y]], r=r)[0]
+        is_selected[selected_idx] = True
+        keywords = self.extract_keywords(is_selected, self.n_keywords_dynamic)
+
+        lens = plt.Circle((x, y), r, edgecolor='black', fill=False)
+        self.ax.add_artist(lens)
+
+        face_colors = [(0.5, 0.5, 0.5, 1) if s else (0.5, 0.5, 0.5, 0.4) for s in is_selected]
+        edge_colors = [(0, 0, 0, 1) if s else (0, 0, 0, 0.4) for s in is_selected]
+        self.scatter_plot.set_facecolor(face_colors)
+        self.scatter_plot.set_edgecolor(edge_colors)
+
+        dy = -1.2 * self.DYNM_FONT_SIZE * self.n_keywords_dynamic / 2
+        for i, keyword in enumerate(keywords):
+            self.ax.annotate(keyword, (x - r, y), (0, dy), textcoords='offset points', ha='right',
+                             va='center', fontsize=self.DYNM_FONT_SIZE,
+                             bbox=dict(boxstyle='square,pad=0.1', fill=False),
+                             color='black')
+            dy += 1.2 * self.DYNM_FONT_SIZE
+
+        return self.ax
 
 
 class LemmaTokenizer(object):
