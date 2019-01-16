b852742523ab0bd0062bacffb92963fd5f4207f7
Simon Janezic <simon.janezic@gmail.com>
2019-01-16 13:47:14 +0100

    Implement label extraction for continuous attributes using t-test

--- lib/tab_exploration/tab_explorer.py
+++ lib/tab_exploration/tab_explorer.py
@@ -1,10 +1,12 @@
 from lib.base_explorer import BaseExplorer
+from scipy.stats import ttest_ind
+import numpy as np
 
 
 class TabExplorer(BaseExplorer):
     """ Visualisation tool for static and dynamic exploration of tabular dataset projection. """
 
-    def __init__(self, p_threshold=0.1, max_static_labels=3, max_dynamic_labels=5, fig_size=(12, 10)):
+    def __init__(self, p_threshold=0.01, max_static_labels=3, max_dynamic_labels=5, fig_size=(12, 10)):
         """
         :param p_threshold: statistical significance (p-value) threshold for annotations
         :param max_static_labels: maximum number of static labels per cluster.
@@ -13,7 +15,8 @@ class TabExplorer(BaseExplorer):
         """
 
         self.p_threshold = p_threshold
-        self.df = None
+        self.df_numeric = None
+        self.df_discrete = None
 
         super().__init__(max_static_labels=max_static_labels, max_dynamic_labels=max_dynamic_labels, fig_size=fig_size)
 
@@ -28,7 +31,8 @@ class TabExplorer(BaseExplorer):
 
         super().fit(df, X_em, clusters, ax=ax)
 
-        self.df = df
+        self.df_numeric = df.select_dtypes(include=[np.number]).astype(np.float32)
+        self.df_discrete = df.select_dtypes(include=[object, 'category', 'bool']).astype(object)
 
     def _extract_labels(self, is_in_cluster, max_labels):
         """
@@ -37,4 +41,20 @@ class TabExplorer(BaseExplorer):
         :param max_labels: maximum number of labels to extract
         :return: list of labels
         """
-        return []
+        # t-test for continuous attributes
+        numeric_in_cluster = self.df_numeric.loc[is_in_cluster].values
+        numeric_out_cluster = self.df_numeric.loc[~is_in_cluster].values
+        # TODO: we only have to compute means and stds once and use the other ttest call
+        _, p_values = ttest_ind(numeric_in_cluster, numeric_out_cluster, equal_var=False)
+
+        p_order = np.argsort(p_values)
+        threshold_idx = np.searchsorted(p_values, self.p_threshold, sorter=p_order, side='right')
+        selected_idx = p_order[:min(threshold_idx, max_labels)]
+
+        # generate labels
+        selected_columns = self.df_numeric.columns.values[selected_idx]
+        avg_in_cluster = np.mean(numeric_in_cluster[:, selected_idx], axis=0)
+        avg_out_cluster = np.mean(numeric_out_cluster[:, selected_idx], axis=0)
+
+        return ['{} = {:.2f} ({})'.format(c, avg_in, 'high' if avg_in > avg_out else 'low')
+                for c, avg_in, avg_out in zip(selected_columns, avg_in_cluster, avg_out_cluster)]
