d813cda1f9282f44f5bb75859de7b49e8365517d
Simon Janežič <simon.janezic@gmail.com>
2019-02-14 11:26:25 +0100

    Add option to choose between one sample and two sample tests.

--- lib/tab_exploration/tab_explorer.py
+++ lib/tab_exploration/tab_explorer.py
@@ -1,40 +1,44 @@
 from lib.base_explorer import BaseExplorer
-from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind, ttest_1samp
 import numpy as np
 from lib.utils.statistics import Hypergeometric, p_adjust_bh
 import pandas as pd
 
+#TODO: use normal indexing instead of bool indexing... a bit faster
 
 # TODO: one way to make these methods more efficient is to take into account the fact that we're dynamically exploring
 # nearby points and not just any points. One could use structures like kd-trees or similiar and store relevant
 # aggregates in the nodes.
 
-# TODO : try one-sample t-test or t-test for all
-
 class TabExplorer(BaseExplorer):
     """ Visualisation tool for static and dynamic exploration of tabular dataset projection. """
 
-    def __init__(self, p_threshold=0.01, representative_threshold=0.5, fdr_correction=True, max_static_labels=3,
-                 max_dynamic_labels=5, min_cluster_size=5, max_discrete_values=3, fig_size=(12, 10)):
+    def __init__(self, p_threshold=0.01, representative_threshold=0.5, use_two_sample_test=False, fdr_correction=True,
+                 max_static_labels=3, max_dynamic_labels=5, min_cluster_size=5, max_discrete_values=3, fig_size=(12, 10)):
         """
         :param p_threshold: statistical significance (p-value) threshold for annotations
         :param representative_threshold: only show labels that represent at least this proportion of samples in a group.
                                          Addresses the representativeness vs significance problem.
+        :param use_two_sample_test: whether to use two sample tests (t-test) or one sample tests
+                                    (hypergeometric test for discrete values and t-test for continuous values)
         :param fdr_correction: whether to apply FDR correction to p-values (multiple comparisons problem)
         :param max_static_labels: maximum number of static labels per cluster
-        :param max_dynamic_labels: maximum number of dynamic labels (shown for selected group of examples)
-        :param max_discrete_values: maximum number of values shown for discrete attributes
+        :param max_dynamic_labels: maximum number of dynamic labels (shown for group of examples selected with a lens)
+        :param max_discrete_values: maximum number of different values shown per discrete attribute
         :param fig_size: Figure size
         """
 
         self.p_threshold = p_threshold
         self.representative_threshold = representative_threshold
+        self.use_two_sample_test = use_two_sample_test
         self.fdr_correction = fdr_correction
         self.max_discrete_values = max_discrete_values
         self.df_numeric = None
         self.df_discrete = None
+        self.numeric_means = None
         self.discrete_columns = None
         self.discrete_value_counts = None
+        self.discrete_value_means = None
         self.hypergeom = None
 
         super().__init__(max_static_labels=max_static_labels, max_dynamic_labels=max_dynamic_labels,
@@ -51,15 +55,16 @@ class TabExplorer(BaseExplorer):
         super().fit(df, X_em, clusters)
 
         self.df_numeric = df.select_dtypes(include=[np.number]).astype(np.float32)
+        self.numeric_means = self.df_numeric.mean()
 
         self.df_discrete = df.select_dtypes(include=[object, 'category', 'bool']).astype(object)
         if not self.df_discrete.empty:
             self.df_discrete = pd.concat([pd.get_dummies(self.df_discrete[col]) for col in self.df_discrete], axis=1,
                                          keys=self.df_discrete.columns)
             self.discrete_value_counts = self.df_discrete.sum()
+            self.discrete_value_means = self.df_discrete.mean()
 
         self.hypergeom = Hypergeometric(max=df.shape[0])
-
 
     def _numeric_p_values(self, is_in_cluster):
         """
@@ -71,9 +76,12 @@ class TabExplorer(BaseExplorer):
             return None
 
         numeric_in_cluster = self.df_numeric.loc[is_in_cluster]
-        numeric_out_cluster = self.df_numeric.loc[~is_in_cluster]
         # TODO: we only have to compute means and stds once and use the other ttest call
-        _, numeric_p_values = ttest_ind(numeric_in_cluster.values, numeric_out_cluster.values, equal_var=False)
+        if self.use_two_sample_test:
+            numeric_out_cluster = self.df_numeric.loc[~is_in_cluster]
+            _, numeric_p_values = ttest_ind(numeric_in_cluster.values, numeric_out_cluster.values, equal_var=False)
+        else:
+            _, numeric_p_values = ttest_1samp(numeric_in_cluster.values, self.numeric_means.values)
 
         # use MultiIndex for better compatibility with discrete p-values Series
         return pd.Series(numeric_p_values,
@@ -89,13 +97,22 @@ class TabExplorer(BaseExplorer):
             return None
 
         total_size = self.df_discrete.shape[0]
-        cluster_size = np.sum(is_in_cluster)
-        discrete_in_cluster_counts = self.df_discrete.loc[is_in_cluster].sum()
-        discrete_p_values = [self.hypergeom.p_value(dc, total_size, vc, cluster_size)
-                             for vc, dc in zip(self.discrete_value_counts, discrete_in_cluster_counts)]
-        discrete_p_values = pd.Series(discrete_p_values, index=discrete_in_cluster_counts.index)
+        discrete_in_cluster = self.df_discrete.loc[is_in_cluster]
 
-        return discrete_p_values
+        if self.use_two_sample_test:
+            discrete_out_cluster = self.df_discrete.loc[~is_in_cluster]
+            ts, discrete_p_values = ttest_ind(discrete_in_cluster.values, discrete_out_cluster.values, equal_var=False)
+            #convert to one-tailed t-test p-values since we're only looking for over-representations
+            t_pos = ts>0
+            discrete_p_values[t_pos] /= 2
+            discrete_p_values[~t_pos] = 1 - discrete_p_values[~t_pos]/2
+        else:
+            discrete_in_cluster_counts = discrete_in_cluster.sum()
+            cluster_size = np.sum(is_in_cluster)
+            discrete_p_values = [self.hypergeom.p_value(dc, total_size, vc, cluster_size)
+                                 for vc, dc in zip(self.discrete_value_counts, discrete_in_cluster_counts)]
+
+        return pd.Series(discrete_p_values, index=self.df_discrete.columns)
 
     #TODO: refactor
     def _extract_labels(self, is_in_cluster, max_labels):
